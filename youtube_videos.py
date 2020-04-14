09aa9a7ba800428a77b622697167fcd299de8844
Louis Spencer <louisspencer@louiss-air.home>
2020-04-14 03:58:05 -0700

    first commit, set up API call function

--- youtube_videos.py
+++ youtube_videos.py
@@ -2,31 +2,28 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 
-DEVELOPER_KEY = "YOURKEYHERE!"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-
-
-
-def youtube_search(q, max_results=50,order="relevance", token=None, location=None, location_radius=None):
+def youtube_search(q, max_results=50,order="relevance",
+                   token=None, location=None, location_radius=None,
+                   videoCategoryId = None, DEVELOPER_KEY = ''):
 
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
   search_response = youtube.search().list(
     q=q,
+    regionCode = 'US',
     type="video",
+    videoCategoryId = videoCategoryId,
     pageToken=token,
     order = order,
     part="id,snippet",
     maxResults=max_results,
     location=location,
     locationRadius=location_radius
-
-  ).execute()
-
-
+    ).execute()
 
   videos = []
 
